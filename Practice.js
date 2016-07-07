df9928e6ec12fa5e1e859264440371a780061633
mari300 <jahfly9@gmail.com>
2016-07-07 18:23:00 +0000

    changes

---
+++ Practice.js
@@ -0,0 +1,15 @@
+$(document).ready();
+var address = $("#address").val();
+var foods = $("#foods").val();
+ foods <= 0$(".=#btn btn-primary").click(function() {
+             alert("Type your location and what you're craving to continue!");
+
+        }
+  
+   // if both of the search bars don't have anything in them, alert the user to type something into both$(".btn btn-primary").click(     
+}
+
+    );
+
+
+// dsjnd 
