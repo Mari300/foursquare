3eee43bd352da6f06333504c838ff2967d5aa839
mari300 <jahfly9@gmail.com>
2016-07-08 18:09:30 +0000

    Nearly Done

--- Practice.js
+++ Practice.js
@@ -1,17 +1,48 @@
 $(document).ready();
 var address = $("#address").val();
-var foods = $("#foods").val();
+var food = $("#food").val();
 
- if (address && foods <= 0) {
-     $(".btn btn-primary").click(function() {
-             alert("Type your location and what you're craving to continue!");
+//  if (address && food <= "") {
+//      $("#search").click(function() {
+ //      alert("Type your location and what you're craving to continue!")
+ //    }); 
 
-        }
-  
-   // if both of the search bars don't have anything in them, alert the user to type something into both$(".btn btn-primary").click(     
+function searchClick() {
+    alert();
+}
+
+// Clear Button  
+$("#clear").click(function() {
+    $("#address").add("#food").val("");
+});
+
+// When Search Button is Clicked, Results Window appears.
+$("#search").click(function() {
+        $("#results").show();
+        $(".jumbotron").hide();
+    }
+);
+
+//Hides Results Window 
+$("#searchAgain").click(function() {
+    
+        $("#results").hide();
+        $(".jumbotron").show();
+
+    });
 
 
-    );
+
+// client id and secret variables 
+var CLIENT_ID = ("I3RX34OWTKGUZKEWXP0OWUBE4EFESC0YPCK0FEZPRODPQN0S");
+var CLIENT_SECRET = ("U3WL0AKRUPTRGVTZ4EMGGKWCC2Z2D5FGV5WNDUUB0EOKJNIU");
+
+// using api for search button
+("#search").getJSON("https://api.foursquare.com/v2/venues/search?client_id=" + "I3RX34OWTKGUZKEWXP0OWUBE4EFESC0YPCK0FEZPRODPQN0S" + "&client_secret=" +"U3WL0AKRUPTRGVTZ4EMGGKWCC2Z2D5FGV5WNDUUB0EOKJNIU"  + "&v=20130815&ll=40.7,-74&query=" + food );
 
 
-// dsjnd 
+//"l,data,success(data,status,xhr)
+
+var answers = {
+ response
+}
